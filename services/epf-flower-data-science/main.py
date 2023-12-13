c1aa26368bb72d113f27f816aab807d7606c4cb9
adozz78 <adrien.ozanne@gmail.com>
2023-12-13 14:54:02 +0100

    Step 3 : Redirect the root endpoint of your API to the automatic swagger documentation

--- services/epf-flower-data-science/main.py
+++ services/epf-flower-data-science/main.py
@@ -1,8 +1,14 @@
 import uvicorn
 
 from src.app import get_application
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = get_application()
+
+@app.get("/")
+def redirect_to_docs():
+    return RedirectResponse(url = "/docs")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
